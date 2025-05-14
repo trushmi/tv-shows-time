ffd43f56e6dc0addecb1e91f9aa621183b7f69d8
“Iryna <trushmi415@gmail.com>
2025-05-13 21:28:43 -0700

    import the ui-component library and use it to update the about page

---
+++ src/components/FAQ/Accordion.data.ts
@@ -0,0 +1,81 @@
+const data = [
+  {
+    id: 1,
+    title: "What does this tool do?",
+    content:
+      "This tool helps you calculate the total amount of time you've spent watching your favorite TV shows. It breaks down the total into days, hours, and minutes.",
+  },
+  {
+    id: 2,
+    title: "How do I use the calculator?",
+    content:
+      "You can add TV shows to the calculator in a few ways. Use the search page to find specific shows. Select shows from a curated list of popular titles. You can even use a combination of both methods.",
+  },
+  {
+    id: 3,
+    title: "What if I recognize a show but want more information?",
+    content:
+      "If you see a show you're not entirely sure about, click on the IMDb icon on its poster to view its IMDb page for more details.",
+  },
+  {
+    id: 4,
+    title: "Does the calculator update automatically when I add a show?",
+    content:
+      "Yes, the calculator updates the total time automatically each time you add a TV show.",
+  },
+  {
+    id: 5,
+    title:
+      "Can I specify how many times I've watched a show or which seasons I've seen?",
+    content:
+      "Yes, the tool allows you to specify the number of times you've watched a particular show or indicate the specific seasons you've viewed.",
+  },
+  {
+    id: 6,
+    title: "How do I start over?",
+    content:
+      'If you want to clear your selections and start a new calculation, simply click the "reset" button.',
+  },
+  {
+    id: 7,
+    title: "Where does the calculator get its data?",
+    content:
+      "The calculator uses data from TVMaze to determine the number of seasons, episodes per season, and the average duration of each episode.",
+  },
+  {
+    id: 8,
+    title: "Is the data for ongoing shows up-to-date?",
+    content:
+      "For TV series that are still airing, the data included in the calculations goes up to August 10, 2023.",
+  },
+  {
+    id: 9,
+    title: "How often is the list of TV shows updated?",
+    content: "The curated list of top TV shows is updated every 6 months.",
+  },
+  {
+    id: 10,
+    title: "How often is the search data updated?",
+    content: "The search data, powered by TVMaze, is consistently updated.",
+  },
+  {
+    id: 11,
+    title: "How accurate is the calculated time?",
+    content:
+      "Please note that the calculated time might vary by up to 10% due to potential differences in episode durations or data updates.",
+  },
+  {
+    id: 12,
+    title:
+      "Can I use this tool to calculate the time I've spent watching movies?",
+    content:
+      "No, this tool is specifically designed for calculating the time spent watching TV shows and does not include calculations for movies.",
+  },
+  {
+    id: 13,
+    title: "Who can I contact if I have feedback or questions?",
+    content:
+      "Please feel free to reach out with any feedback or questions you may have at trush94041@gmail.com",
+  },
+];
+export default data;
--- src/components/FAQ/faq.jsx
+++ src/components/FAQ/faq.jsx
@@ -1,59 +1,14 @@
 import React from "react";
 import styles from "./faq.module.scss";
-import { Link } from "react-router-dom";
-
+import { Accordion } from "@trushmi/ui-components";
+import data from "./Accordion.data.ts";
 export default function FAQ() {
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.heading}>
-        Ever wondered how much time you've spent watching TV shows?
-      </h1>
-      <p className={styles.intro}>Let's find out!</p>
-      <ul className={styles.list}>
-        <li>
-          Choose your favorite TV shows, and see the total days, hours, and
-          minutes you've dedicated to them.
-        </li>
-        <li>
-          Wondering how? Use the{" "}
-          <Link to="/search" className={styles.link}>
-            search page
-          </Link>
-          , choose from a{" "}
-          <Link to="/tvshows155" className={styles.link}>
-            curated list of top TV shows
-          </Link>
-          , or mix and match both methods! If a show seems familiar but you
-          can't quite remember it, click on the IMDb icon on its poster for more
-          details.
-        </li>
-        <li>
-          Whenever you add a show, the calculator updates automatically. You can
-          specify the number of times you've watched it or which seasons you've
-          seen. Want to start again? Simply hit the reset button.
-        </li>
-      </ul>
-      <h2 className={styles.subHeading}>How Does The Calculator Work?</h2>
-      <p>
-        It uses{" "}
-        <a href="https://www.tvmaze.com/api" className={styles.emailLink}>
-          TVMaze
-        </a>{" "}
-        data ; The tool considers the number of seasons, episodes in each
-        season, and the average episode duration. For series still on air, data
-        up to August 10, 2023, is included. The list of TV shows receives
-        updates every 6 months, while search data is consistently updated by
-        TVMaze. Please note, the data might vary by up to 10%. This tool is
-        exclusively for calculating time spent on TV shows, not movies.
-      </p>
-      <h2 className={styles.subHeading}>Feedback or Questions?</h2>
-      <p>
-        Eager to help! Reach out at {""}
-        <a href="mailto:trushmi415@gmail.com" className={styles.emailLink}>
-          trushmi415@gmail.com
-        </a>
-        .
-      </p>
+      <Accordion
+        accordionData={data}
+        accordionTitle="Answers to the most popular questions"
+      />
     </div>
   );
 }
